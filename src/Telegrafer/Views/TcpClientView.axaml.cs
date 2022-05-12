0238695eab88403feb85aa9f89d3a0e3afbc009d
Timo Weike <timo_weike@gmx.de>
2022-05-12 13:58:21 +0200

    Use fuzzy match to filter old payloads

--- src/Telegrafer/Views/TcpClientView.axaml.cs
+++ src/Telegrafer/Views/TcpClientView.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using FuzzySharp;
+using FuzzySharp.PreProcess;
 
 namespace Telegrafer.Views
 {
@@ -8,7 +10,25 @@ namespace Telegrafer.Views
     {
         public TcpClientView()
         {
-            InitializeComponent();   
+            InitializeComponent();
+
+            this.box.FilterMode = AutoCompleteFilterMode.Custom;
+            this.box.TextFilter = (s, c) =>
+            {
+                if (c.Contains(s, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                if (Fuzz.Ratio(s, c) > 50)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
         }
     }
 }
